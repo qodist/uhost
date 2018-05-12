61e3319a505b02a1a501262d278f6c83e6ab4e7d
qodist <reyno75@gmail.com>
2018-05-12 19:10:59 +0300

    midway through the responsive section

--- shared.js
+++ shared.js
@@ -2,19 +2,40 @@ var backdrop = document.querySelector(".backdrop");
 var modal = document.querySelector(".modal");
 var planButtons = document.querySelectorAll(".plan button");
 var modalNoButton = document.querySelector(".modal__action--negative");
+var toggleButton = document.querySelector(".toggle-button");
+var mobileNav = document.querySelector(".mobile-nav");
 
 planButtons.forEach(function(button){
     button.addEventListener("click", function(){
-        backdrop.style.display = "block";
-        modal.style.display = "block";
+        // backdrop.style.display = "block";
+        // modal.style.display = "block";
+        backdrop.classList.add("open");
+        modal.classList.add("open");
     });
 })
 
-modalNoButton.addEventListener("click", closeModal);
+backdrop.addEventListener("click", function(){
+    // mobileNav.style.display = "none";
+    mobileNav.classList.remove("open");
+    closeModal();
+});
 
-backdrop.addEventListener("click", closeModal);
+if(modalNoButton){
+    modalNoButton.addEventListener("click", closeModal);
+}
 
 function closeModal(){
-    backdrop.style.display = "none";
-    modal.style.display = "none";
+    // backdrop.style.display = "none";
+    // modal.style.display = "none";
+    backdrop.classList.remove("open");
+    if(modal){
+        modal.classList.remove("open");
+    }
 }
+
+toggleButton.addEventListener("click", function(){
+    // mobileNav.style.display = "block";
+    // backdrop.style.display = "block";
+    mobileNav.classList.add("open");
+    backdrop.classList.add("open");
+});
